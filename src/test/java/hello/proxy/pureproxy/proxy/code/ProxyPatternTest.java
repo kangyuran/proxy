79edb8080ac34392f4ec84bc7c059a0fff5159a4
kangyuran <skyr0917@naver.com>
2022-12-25 17:01:02 +0900

    Initial commit

---
+++ src/test/java/hello/proxy/pureproxy/proxy/code/ProxyPatternTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.pureproxy.proxy.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.cache.annotation.CacheConfig;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
